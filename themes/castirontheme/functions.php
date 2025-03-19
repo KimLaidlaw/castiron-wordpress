e8e4e6f9a6da900c12697f7fb3480b5581db0a23
Keith Laidlaw <keithlaidlaw@gmail.com>
2025-03-19 08:09:39 -0700

    Create stylesheet and functions.php
    And enqueue stylesheet

---
+++ themes/castirontheme/functions.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * functions.php
+ **/
+
+// block direct access
+if ( !defined( 'WPINC' ) ) {
+	exit( 'This file cannot be accessed directly.' );
+}
+
+
+
+// enqueue main theme stylesheet
+function _castiron_enqueue_styles() {
+	wp_enqueue_style(
+		'theme-styles', 
+		get_template_directory_uri() . '/assets/css/style.css', 
+		false, 
+		filemtime( get_template_directory() . '/assets/css/style.css' )
+	);
+}
+add_action('wp_enqueue_scripts', '_castiron_enqueue_styles');
