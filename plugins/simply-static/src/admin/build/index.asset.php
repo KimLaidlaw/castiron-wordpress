e7fd1cf4b0931b07d8dec09f18d7c8cbd392c761
Keith Laidlaw <keithlaidlaw@gmail.com>
2025-09-10 17:03:57 -0700

    Add ACF plugin

--- plugins/simply-static/src/admin/build/index.asset.php
+++ plugins/simply-static/src/admin/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => '2069564e848a9c1ef858');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-components', 'wp-element'), 'version' => 'dbbf48319b68595071f4');
