82c74557d4b6ea13a3edd97e9413e08c7dc49588
Keith Laidlaw <keithlaidlaw@gmail.com>
2025-09-12 15:13:04 -0700

    Add image to hero section

--- themes/castirontheme/front-page.php
+++ themes/castirontheme/front-page.php
@@ -8,31 +8,42 @@
   if(!empty(trim($title_list))) {
     $titles = preg_split('/\s*\R/', rtrim($title_list), NULL, PREG_SPLIT_NO_EMPTY);
   }
+  $image = $hero['image'];
 
 ?>
 
 <!-- front-page.php -->
 <?php if(have_posts()) : ?>
   <?php while(have_posts()): the_post(); ?>
-
     <div class="content">
-      <?php if(!empty(trim($headline))) : ?>
-        <h1>
-          <?php if(!empty(trim($prehead))) : ?>
-            <span class="prehead"><?= $prehead; ?></span> 
-          <?php endif; ?>
-          <?= $headline; ?>
-        </h1>
-      <?php endif; ?>
-      <?php if($titles) : ?>
-        <ul class="title-list">
-          <?php foreach($titles as $title) : ?>
-            <li><?= $title; ?></li>
-          <?php endforeach; ?>
-        </ul>
-        <?php endif; ?>
-    </div>
 
+      <div class="hero-section">
+        <div class="hero-text">
+          <?php if(!empty(trim($headline))) : ?>
+            <h1>
+              <?php if(!empty(trim($prehead))) : ?>
+                <span class="prehead"><?= $prehead; ?></span> 
+              <?php endif; ?>
+              <?= $headline; ?>
+            </h1>
+          <?php endif; ?>
+          <?php if($titles) : ?>
+            <ul class="title-list">
+              <?php foreach($titles as $title) : ?>
+                <li><?= $title; ?></li>
+              <?php endforeach; ?>
+            </ul>
+          <?php endif; ?>
+        </div>
+
+        <div class="hero-image">
+          <?php if (!empty($image)) : ?>
+            <img src="<?php echo $image['url'] ?? null; ?>" alt="<?php echo $image['alt'] ?? null; ?>" />
+          <?php endif; ?>
+        </div>
+      </div>
+
+    </div>
   <?php endwhile; ?>
 <?php endif; ?>
 
